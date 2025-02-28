002fba810ec48f186e89e56030b5ab1d1ad4816e
shaliniblitzy <shalini@blitzy.com>
2025-03-01 02:46:51 +0530

    Add public-samples/page-object-model-using-python-pytest-selenium-55year/src/test/tests/user_registration/__init__.py

---
+++ src/test/tests/user_registration/__init__.py
@@ -0,0 +1,6 @@
+"""
+User Registration test package.
+
+This package contains tests for the User Registration feature of the Storydoc application.
+Tests in this package verify the ability to automate the signup process using mailinator.com emails.
+"""
